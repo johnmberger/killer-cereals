e5d07a04354d87edc323d44a456483b8dd1f69cd
johnmberger <johnmartinberger@gmail.com>
2016-10-20 13:29:43 -0600

    init

---
+++ src/js/components/checkout/checkout.controller.js
@@ -0,0 +1,18 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('myApp.components.checkout', [])
+    .controller('checkoutController', checkoutController);
+
+  checkoutController.$inject = ['$scope', 'cart'];
+
+  function checkoutController($scope, inventory, cart) {
+    /*jshint validthis: true */
+    var vm = this;
+
+    vm.cart = cart.items;
+  }
+
+})();
