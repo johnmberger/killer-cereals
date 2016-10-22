a5d95d0c0faa4e2d2bacbd55585c4c82f083edde
johnmberger <johnmartinberger@gmail.com>
2016-10-22 14:15:38 -0600

    updated views and added toast

--- js/components/main/main.controller.js
+++ js/components/main/main.controller.js
@@ -21,21 +21,25 @@
     vm.cartCount = cart.quantity;
     vm.categories = inventory.categories;
 
+    /* jshint ignore:start */
     vm.addToCart = (item, quantity) => {
       if (item.inStock) {
         if (cart.items.length) {
           for (var i = 0; i < cart.items.length; i++) {
             if (cart.items[i]["_id"] === item["_id"]) {
+              Materialize.toast('Quantity Updated!', 3000);
               return cart.items[i].quantity += quantity;
             }
           }
         }
+        Materialize.toast('Added to cart!', 3000);
         item.quantity = quantity;
         cart.items.push(item);
       } else {
         console.log("item not available!");
       }
     };
+    /* jshint ignore:end */
   }
 
 })();
